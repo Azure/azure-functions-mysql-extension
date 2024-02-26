faa2e722870d6b15ae659c26016c31c4bf5b7d3f
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-02-26 10:46:57 +0530

    Output Binding By using Insert Into Query

---
+++ samples/samples-csharp/OutputBindingSamples/AddProduct.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.MySql.Samples.Common;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.Samples.OutputBindingSamples
+{
+    public static class AddProduct
+    {
+        [FunctionName(nameof(AddProduct))]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "addproduct")]
+            [FromBody] Product prod,
+            [MySql("Products", "MySqlConnectionString")] out Product product)
+        {
+            product = prod;
+            return new CreatedResult($"/api/addproduct", product);
+        }
+    }
+}
---
+++ samples/samples-csharp/OutputBindingSamples/AddProductsAsyncCollector.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.MySql.Samples.Common;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.Samples.OutputBindingSamples
+{
+    public static class AddProductsAsyncCollector
+    {
+        [FunctionName(nameof(AddProductsAsyncCollector))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "addproducts-asynccollector")]
+            HttpRequest req,
+            [MySql("Products", "MySqlConnectionString")] IAsyncCollector<Product> products)
+        {
+            List<Product> newProducts = ProductUtilities.GetNewProducts(5000);
+            foreach (Product product in newProducts)
+            {
+                await products.AddAsync(product);
+            }
+            // Rows are upserted here
+            await products.FlushAsync();
+
+            newProducts = ProductUtilities.GetNewProducts(5000);
+            foreach (Product product in newProducts)
+            {
+                await products.AddAsync(product);
+            }
+            return new CreatedResult($"/api/addproducts-collector", "done");
+        }
+    }
+}
