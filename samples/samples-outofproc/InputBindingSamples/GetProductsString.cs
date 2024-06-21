edab13dc04bc33d14fcd58de608e4946603485f5
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-06-21 09:36:03 +0530

    Add project "samples-outofproc" to solution

---
+++ samples/samples-outofproc/InputBindingSamples/GetProductsString.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Extensions.MySql;
+using Microsoft.Azure.Functions.Worker;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.SamplesOutOfProc.InputBindingSamples
+{
+    public static class GetProductsString
+    {
+        [Function("GetProductsString")]
+        public static string Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getproducts-string/{cost}")]
+            HttpRequestData req,
+            [MySqlInput("select * from Products where cost = @Cost",
+                "MySqlConnectionString",
+                parameters: "@Cost={cost}")]
+            string products)
+        {
+            // Products is a JSON representation of the returned rows. For example, if there are two returned rows,
+            // products could look like:
+            // [{"ProductId":1,"Name":"Dress","Cost":100},{"ProductId":2,"Name":"Skirt","Cost":100}]
+            return products;
+        }
+    }
+}
