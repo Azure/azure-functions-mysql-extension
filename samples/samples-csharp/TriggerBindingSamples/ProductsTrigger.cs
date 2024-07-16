9fa0dc02646c6998fe39510a4264daf3bd72d7f5
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-07-16 17:59:01 +0530

    add Trigger Binding samples

---
+++ samples/samples-csharp/TriggerBindingSamples/ProductsTrigger.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Extensions.MySql.Samples.Common;
+using Microsoft.Azure.WebJobs.Extensions.MySql.TriggersBinding;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.Samples.TriggerBindingSamples
+{
+    public static class ProductsTrigger
+    {
+        [FunctionName(nameof(ProductsTrigger))]
+        public static void Run(
+            [MySqlTrigger("Products", "MySqlConnectionString")]
+            IReadOnlyList<MySqlChange<Product>> changes,
+            ILogger logger)
+        {
+            // The output is used to inspect the trigger binding parameter in test methods.
+            logger.LogInformation("MySQL Changes: " + JsonConvert.SerializeObject(changes));
+        }
+    }
+}
---
+++ samples/samples-csharp/TriggerBindingSamples/ProductsTriggerLeasesTableName.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Extensions.MySql.Samples.Common;
+using Microsoft.Azure.WebJobs.Extensions.MySql.TriggersBinding;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.Samples.TriggerBindingSamples
+{
+    public static class ProductsTriggerLeasesTableName
+    {
+        [FunctionName(nameof(ProductsTriggerLeasesTableName))]
+        public static void Run(
+            [MySqlTrigger("Products", "MySqlConnectionString", "Leases")]
+            IReadOnlyList<MySqlChange<Product>> changes,
+            ILogger logger)
+        {
+            logger.LogInformation("MySQL Changes: " + JsonConvert.SerializeObject(changes));
+        }
+    }
+}
