474e3a34f2126b3ea38dc683380643300a4c09bd
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2024-08-19 15:04:23 +0530

    added samples for trigger binding, even for out-of-proc

---
+++ Worker.Extensions.MySql/MySqlTriggerAttribute.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.MySql
+{
+    public sealed class MySqlTriggerAttribute : TriggerBindingAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySqlTriggerAttribute"/> class, which triggers the function when any changes on the specified table are detected.
+        /// </summary>
+        /// <param name="tableName">Name of the table to watch for changes.</param>
+        /// <param name="connectionStringSetting">The name of the app setting where the MySQL connection string is stored</param>
+        /// <param name="leasesTableName">Optional - The name of the table used to store leases. If not specified, the leases table name will be Leases_{FunctionId}_{TableId}</param>
+        public MySqlTriggerAttribute(string tableName, string connectionStringSetting, string leasesTableName = null)
+        {
+            this.TableName = tableName ?? throw new ArgumentNullException(nameof(tableName));
+            this.ConnectionStringSetting = connectionStringSetting ?? throw new ArgumentNullException(nameof(connectionStringSetting));
+            this.LeasesTableName = leasesTableName;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySqlTriggerAttribute"/> class with null value for LeasesTableName.
+        /// </summary>
+        /// <param name="tableName">Name of the table to watch for changes.</param>
+        /// <param name="connectionStringSetting">The name of the app setting where the MySQL connection string is stored</param>
+        public MySqlTriggerAttribute(string tableName, string connectionStringSetting) : this(tableName, connectionStringSetting, null) { }
+
+        /// <summary>
+        /// Name of the app setting containing the MySQL connection string.
+        /// </summary>
+        public string ConnectionStringSetting { get; }
+
+        /// <summary>
+        /// Name of the table to watch for changes.
+        /// </summary>
+        public string TableName { get; }
+
+        /// <summary>
+        /// Name of the table used to store leases.
+        /// If not specified, the leases table name will be Leases_{FunctionId}_{TableId}
+        /// </summary>
+        public string LeasesTableName { get; }
+    }
+}
---
+++ samples/samples-outofproc/TriggerBindingSamples/ProductsTrigger.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.MySql;
+using Microsoft.Azure.WebJobs.Extensions.MySql.SamplesOutOfProc.Common;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.MySql.SamplesOutOfProc.TriggerBindingSamples
+{
+    public class ProductsTrigger
+    {
+        private static readonly Action<ILogger, string, Exception> _loggerMessage = LoggerMessage.Define<string>(LogLevel.Information, eventId: new EventId(0, "INFO"), formatString: "{Message}");
+
+        [Function(nameof(ProductsTrigger))]
+        public static void Run(
+            [MySqlTrigger("Products", "MySqlConnectionString")]
+            IReadOnlyList<MySqlChange<Product>> changes, FunctionContext context)
+        {
+            // The output is used to inspect the trigger binding parameter in test methods.
+            if (changes != null && changes.Count > 0)
+            {
+                _loggerMessage(context.GetLogger("ProductsTrigger"), "MySQL Changes: " + JsonConvert.SerializeObject(changes), null);
+            }
+        }
+    }
+}
