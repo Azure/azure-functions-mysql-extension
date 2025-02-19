e2584ab4a6a799a7c3be2c10cf6af80b9bdfb322
Sumit Kumar <107923889+sumitkumams@users.noreply.github.com>
2025-02-19 15:35:12 +0530

    Update Trigger Binding Samples

--- samples/samples-csharp/TriggerBindingSamples/ProductsTriggerJson.cs
+++ samples/samples-csharp/TriggerBindingSamples/ProductsTriggerJson.cs
@@ -1,27 +1,23 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
 using Microsoft.Azure.WebJobs.Extensions.MySql.Samples.Common;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Microsoft.Azure.WebJobs.Extensions.MySql.Samples.TriggerBindingSamples
 {
-    public static class ProductsTrigger
+    public static class ProductsTriggerJson
     {
-        [FunctionName(nameof(ProductsTrigger))]
+        [FunctionName(nameof(ProductsTriggerJson))]
         public static void Run(
             [MySqlTrigger("Products", "MySqlConnectionString")]
             IReadOnlyList<MySqlChange<Product>> changes,
             ILogger logger)
         {
             // The output is used to inspect the trigger binding parameter in test methods.
-            foreach (MySqlChange<Product> change in changes)
-            {
-                Product product = change.Item;
-                logger.LogInformation($"Change operation: {change.Operation}");
-                logger.LogInformation($"Product Id: {product.ProductId}, Name: {product.Name}, Cost: {product.Cost}");
-            }
+            logger.LogInformation("MySQL Changes: " + JsonConvert.SerializeObject(changes));
         }
     }
 }
--- samples/samples-outofproc/TriggerBindingSamples/ProductsTriggerJson.cs
+++ samples/samples-outofproc/TriggerBindingSamples/ProductsTriggerJson.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -6,28 +6,25 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.MySql;
 using Microsoft.Azure.WebJobs.Extensions.MySql.SamplesOutOfProc.Common;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 
 namespace Microsoft.Azure.WebJobs.Extensions.MySql.SamplesOutOfProc.TriggerBindingSamples
 {
-    public class ProductsTrigger
+    public class ProductsTriggerJson
     {
         private static readonly Action<ILogger, string, Exception> _loggerMessage = LoggerMessage.Define<string>(LogLevel.Information, eventId: new EventId(0, "INFO"), formatString: "{Message}");
 
-        [Function(nameof(ProductsTrigger))]
+        [Function(nameof(ProductsTriggerJson))]
         public static void Run(
             [MySqlTrigger("Products", "MySqlConnectionString")]
             IReadOnlyList<MySqlChange<Product>> changes, FunctionContext context)
         {
-            ILogger logger = context.GetLogger("ProductsTrigger");
             // The output is used to inspect the trigger binding parameter in test methods.
-            foreach (MySqlChange<Product> change in changes)
+            if (changes != null && changes.Count > 0)
             {
-                Product product = change.Item;
-                _loggerMessage(logger, $"Change operation: {change.Operation}", null);
-                _loggerMessage(logger, $"Product Id: {product.ProductId}, Name: {product.Name}, Cost: {product.Cost}", null);
+                _loggerMessage(context.GetLogger("ProductsTrigger"), "MySQL Changes: " + JsonConvert.SerializeObject(changes), null);
             }
-
         }
     }
 }
